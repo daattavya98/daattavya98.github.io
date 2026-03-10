43d65c54326db20b94e1d77365a195c51d7d34a0
daattavya98 <da579@cam.ac.uk>
2026-03-10 13:08:38 +0000

    Deploying to gh-pages from @ daattavya98/daattavya98.github.io@122c88ac0b3ff29be7a906cb94f5a3e31add9919 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-blog",
           title: "blog",
-          description: "A reading list of blogs and webpages on AI for mathematics.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/blog/";
